bc1cca2671848ef0bb31bd13c79e0e430b447692
Iwoobi <cocoduddnd@naver.com>
2021-11-23 01:44:46 +0900

    asd

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,35 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youhan <youhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/18 18:50:36 by youhan            #+#    #+#             */
-/*   Updated: 2021/11/18 18:50:36 by youhan           ###   ########.fr       */
+/*   Created: 2021/11/18 21:13:50 by youhan            #+#    #+#             */
+/*   Updated: 2021/11/18 21:13:50 by youhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <string.h>
-void	ft_memcpy(void *dest, const void *source, unsigned int num)
+
+void *memmove (void *dest, const void *src, unsigned int num)
 {
 	char		*dest_point;
-	const char	*source_point;
+	const char	*src_point;
 
 	dest_point = dest;
-	source_point = source;
-	while (num > 0)
+	src_point = src;
+	if (dest_point < src_point)
 	{
-		dest_point[num - 1] = source_point[num - 1];
-		num--;
+		while (num > 0)
+			*dest_point = *src_point;
 	}
-}
-int main()
-{
-	char arr[10]="1234567";
-	char arr1[10]="1234567";
-	ft_memcpy(arr+1,arr,12);
-	memcpy(arr1+1,arr1,12);
-	printf("my:%s",arr);
-	printf("or:%s",arr1);
+	else
+	{
+		int i;
+
+		i = 0;
+		while (num > i)
+		{
+			*dest_point = *src_point;
+			i++;
+		}
+	}
 }
