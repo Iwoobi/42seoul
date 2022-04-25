c7076f54e936abd54a9d9057aed4ac82062b2947
Youngung Han <youhan@c5r7s4.42seoul.kr>
2022-04-25 22:43:33 +0900

    asd

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: youhan <youhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 21:45:15 by youhan            #+#    #+#             */
-/*   Updated: 2022/04/22 20:29:08 by youhan           ###   ########.fr       */
+/*   Updated: 2022/04/25 22:43:15 by youhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
 typedef struct s_list
 {
 	int				val;
 	struct s_list	*before;
 	struct s_list	*next;
 }	t_list;
-
-int	ft_atoi(char *input)
+void	ft_swap(int *a, int *b)
 {
-	int	sign;
-	int	n;
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	return ;
+}
+
+void	ft_quicksort(int *arr, int L, int R)
+{
+	int	left;
+	int	right;
+	int	pivot;
+
+	left = L;
+	right = R;
+	pivot = arr[(L + R) / 2];
+	while (left <= right)
+	{
+		while (arr[left] < pivot)
+			left++;
+		while (arr[right] > pivot)
+			right--;
+		if (left <= right)
+		{
+			ft_swap(&arr[left], &arr[right]);
+			left++;
+			right--;
+		}
+	}
+	if (L < right)
+		ft_quicksort(arr, L, right);
+	if (R > left)
+		ft_quicksort(arr, left, R);
+}
+
+long long	ft_atoi(char *str)
+{
+	int			sign;
+	long long	n;
 
 	sign = 1;
 	n = 0;
@@ -46,7 +82,137 @@ int	ft_atoi(char *input)
 	return (n * sign);
 }
 
+int	ft_check_input_number(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		if (argv[i][0] == '-')
+			j++;
+		while (argv[i][j])
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				return (-1);
+			j++;
+		}
+		if (j > 11)
+			return (-1);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_input_unique(int *arr, int argc)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc - 1)
+		{
+			if (arr[i] == arr[j])
+				return (-1);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_input_number_push(int argc, char **argv, int **sort_arr)
+{
+	int	i;
+
+	if (ft_check_input_number(argc, argv) == -1)
+		return (-1);
+	*sort_arr = (int *)malloc(sizeof(int) * (argc - 1));
+	if (!(*sort_arr))
+		return (-1);
+	i = 0;
+	while (i < argc - 1)
+	{
+		if (ft_atoi(argv[1 + i]) > 2147483647)
+			return (-1);
+		if (ft_atoi(argv[i + 1]) < -2147483648)
+			return (-1);
+		(*sort_arr)[i] = (int)ft_atoi(argv[1 + i]);
+		i++;
+	}
+	if (ft_input_unique(*sort_arr, argc) == -1)
+		return (-1);
+	return (1);
+}
+
+int	ft_input_number_index(int *arr, char **argv, int len, int i)
+{
+	int	j;
+
+	j = 0;
+	while (j < len)
+	{
+		if (arr[i] == ft_atoi(argv[1 + j]))
+			return (1 + j);
+		j++;
+	}
+	return (-1);
+}
+
+t_list	*ft_make_stack(t_list **stack_n, int *arr, char **argv ,int len)
+{
+	int	i;
+	t_list	*save_add;
+
+	save_add = *stack_n;
+	i = 0;
+	while (i < len)
+	{
+		(*stack_n)->next = malloc(sizeof(t_list));
+		if ((*stack_n)->next == NULL)
+			return (NULL);
+		(*stack_n)->val = ft_input_number_index(arr, argv, len, i);
+		if ((*stack_n)->next == NULL)
+			return (NULL);
+		((*stack_n)->next)->before = (*stack_n);
+		*stack_n = (*stack_n)->next;
+		i++;
+	}
+	return (save_add);
+}
+void	ft_printf_list(t_list *stack)
+{
+	while (stack->next != NULL)
+	{
+		printf("%d\n",stack->val);
+		stack = stack->next;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc =)
+	int	*sort_arr;
+
+	if (argc < 3)
+		return (-1);
+	if (ft_input_number_push(argc, argv, &sort_arr) == -1)
+		return (-1);
+	ft_quicksort(sort_arr, 0, argc - 2);
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	stack_a = (t_list *)malloc(sizeof(t_list));
+	stack_b = (t_list *)malloc(sizeof(t_list)); 
+	stack_a->next = NULL;
+	stack_b->before = NULL;
+
+	stack_a = ft_make_stack(&stack_a, sort_arr, argv, argc - 1);
+	ft_printf_list(stack_a);
+
+
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,17 +6,12 @@
 /*   By: youhan <youhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 19:58:31 by youhan            #+#    #+#             */
-/*   Updated: 2022/04/22 20:29:08 by youhan           ###   ########.fr       */
+/*   Updated: 2022/04/25 22:27:07 by youhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct s_list
-{
-	int				val;
-	struct s_list	*before;
-	struct s_list	*next;
-}	t_list;
+
 #endif
