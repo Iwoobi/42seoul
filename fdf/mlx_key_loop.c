979bf0058559366d9c756f5f6e5bb5a60a9034cf
Iwoobi <cocoduddnd@naver.com>
2022-05-27 03:27:36 +0900

    asd

--- fdf/mlx_key_loop.c
+++ fdf/mlx_key_loop.c
@@ -30,59 +30,71 @@ void	ft_mlx_img_clear(t_mlx *my_mlx)
 	}
 }
 
-int	deal_key(int key_code, t_mlx *my_mlx)
+int	deal_key_var3(int key_code, t_mlx *my_mlx)
 {
-	if (key_code == 53)
-		exit(0);
-	if (key_code == 123)
-	{
-		ft_mlx_img_clear(my_mlx);
-		my_mlx->data->data_c.theta += 1;
-	}
-	if (key_code == 124)
-	{
-		ft_mlx_img_clear(my_mlx);
-		my_mlx->data->data_c.theta -= 1;
-	}
-	if (key_code == 125)
-	{
-		my_mlx->data->data_c.alpha -= 3;
-		ft_mlx_img_clear(my_mlx);
-	}
-	if (key_code == 126)
-	{
-		my_mlx->data->data_c.alpha += 3;
-		ft_mlx_img_clear(my_mlx);
-	}
-	if (key_code == 12)
-	{
-		ft_mlx_img_clear(my_mlx);
-		my_mlx->data->data_c.mid_x += 0.3 * my_mlx->data->len;
-	}
-	if (key_code == 13)
-	{
-		ft_mlx_img_clear(my_mlx);
-		my_mlx->data->data_c.mid_x -= 0.3 * my_mlx->data->len;
-	}
 	if (key_code == 14)
 	{
 		my_mlx->data->data_c.mid_y -= 0.3 * my_mlx->data->len;
 		ft_mlx_img_clear(my_mlx);
 	}
-	if (key_code == 15)
+	else if (key_code == 15)
 	{
 		my_mlx->data->data_c.mid_y += 0.3 * my_mlx->data->len;
 		ft_mlx_img_clear(my_mlx);
 	}
-	if (key_code == 16)
+	else if (key_code == 16)
 	{
 		my_mlx->data->mul += 0.1;
 		ft_mlx_img_clear(my_mlx);
 	}
-	if (key_code == 17)
+	else if (key_code == 17)
 	{
 		my_mlx->data->mul -= 0.1;
 		ft_mlx_img_clear(my_mlx);
 	}
+	return (0);
+}
+
+int	deal_key_var2(int key_code, t_mlx *my_mlx)
+{
+	if (key_code == 126)
+	{
+		my_mlx->data->data_c.alpha += 3;
+		ft_mlx_img_clear(my_mlx);
+	}
+	else if (key_code == 12)
+	{
+		ft_mlx_img_clear(my_mlx);
+		my_mlx->data->data_c.mid_x += 0.3 * my_mlx->data->len;
+	}
+	else if (key_code == 13)
+	{
+		ft_mlx_img_clear(my_mlx);
+		my_mlx->data->data_c.mid_x -= 0.3 * my_mlx->data->len;
+	}
+	return (0);
+}
+
+int	deal_key(int key_code, t_mlx *my_mlx)
+{
+	if (key_code == 53)
+		exit(0);
+	else if (key_code == 123)
+	{
+		ft_mlx_img_clear(my_mlx);
+		my_mlx->data->data_c.theta += 1;
+	}
+	else if (key_code == 124)
+	{
+		ft_mlx_img_clear(my_mlx);
+		my_mlx->data->data_c.theta -= 1;
+	}
+	else if (key_code == 125)
+	{
+		my_mlx->data->data_c.alpha -= 3;
+		ft_mlx_img_clear(my_mlx);
+	}
+	else
+		deal_key_var2(key_code, my_mlx);
 	return (0);
 }
